30a579aa3973cc39984e66790ed2c63458569ce6
giatechconsumerwarehouse-crypto <giatechconsumerwarehouse@gmail.com>
2025-09-29 00:03:27 +0100

    Create tracking.js

---
+++ admin/pages/tracking.js
@@ -0,0 +1,8 @@
+export default function Tracking() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial, sans-serif" }}>
+      <h1>📍 Shipment Tracking</h1>
+      <p>View live location updates of your shipments in the Philippines.</p>
+    </div>
+  );
+}
