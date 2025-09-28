39167e2d4a12998df5e77b79b955b358c3d96629
giatechconsumerwarehouse-crypto <giatechconsumerwarehouse@gmail.com>
2025-09-28 23:25:43 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚚 Welcome to Giatech Shipping</h1>
+      <p>Track your shipments easily with live updates.</p>
+      <p>Admin and Client Portal</p>
+    </div>
+  );
+}
