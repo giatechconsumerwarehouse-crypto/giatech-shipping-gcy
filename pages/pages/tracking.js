38336188b27c5fa2d83f48fcaadb4fd0b87b24ed
giatechconsumerwarehouse-crypto <giatechconsumerwarehouse@gmail.com>
2025-09-28 23:30:31 +0100

    Create tracking.js

---
+++ pages/pages/tracking.js
@@ -0,0 +1,8 @@
+export default function Tracking() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>📍 Shipment Tracking</h1>
+      <p>Enter your tracking number to see live shipment updates.</p>
+    </div>
+  );
+}
